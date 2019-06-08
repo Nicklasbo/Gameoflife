cec63eba3c2b6563d62eef166a5e56e2a2aaafcf
Nicklas Embo <emmerbo@gmail.com>
2019-06-08 16:57:08 +0200

    remade and added second rule plus todo

---
+++ cfg.py
@@ -0,0 +1,9 @@
+# Define the colors we will use in RGB format
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Set the height and width of the screen
+size = [800, 800]
+
+# Sets the initial mouse position
+mouse_pos = (0, 0)
